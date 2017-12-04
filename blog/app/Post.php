966f68ff21cbd8657bc5af37154645de74a318af
Jordan Center <BigJman1234567@yahoo.com>
2017-12-04 10:44:15 -0600

    finished add comments

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -4,5 +4,11 @@ namespace App;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'body'];
+    public function comments() {
+    	return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body) {
+    	$this->comments()->create(compact('body'));
+    }
 }
