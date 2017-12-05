7c4daf657d4d89a4072855eeac83c811868afdba
Jordan Center <BigJman1234567@yahoo.com>
2017-12-05 12:05:46 -0600

    finished the service container

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,15 +1,9 @@
 <?php
+	
+App::singleton('App\Billing\Stripe', function() {
+	return  new \App\Billing\Stripe(config('services.stripe.secret'));
+});
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', 'PostsController@index')->name('home');
 Route::get('/posts/create', 'PostsController@create');
